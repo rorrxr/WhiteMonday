0392d22aa0ccbf8a8f78a83a944d0b73f68b736d
muchluvu <rorrxr@naver.com>
2024-12-18 16:44:59 +0900

    Initial commit

---
+++ src/main/java/com/minju/whitemonday/WhiteMondayApplication.java
@@ -0,0 +1,13 @@
+package com.minju.whitemonday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiteMondayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhiteMondayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/minju/whitemonday/WhiteMondayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minju.whitemonday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiteMondayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
