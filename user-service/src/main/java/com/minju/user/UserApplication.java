765a953bdb2115d70cb5829661d7bde0fa143e37
muchluvu <rorrxr@naver.com>
2025-01-06 21:50:44 +0900

    fix: UserApplication에 @EnableDiscoveryClient 추가

--- user-service/src/main/java/com/minju/user/UserApplication.java
+++ user-service/src/main/java/com/minju/user/UserApplication.java
@@ -1,6 +1,5 @@
 package com.minju.user;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -11,8 +10,6 @@ public class UserApplication {
 
 	public static void main(String[] args) {
 
-//		Dotenv dotenv = Dotenv.configure().load();
-//		System.setProperty("JWT_SECRET_KEY", dotenv.get("JWT_SECRET_KEY"));
 		SpringApplication.run(UserApplication.class, args);	}
 
 }
