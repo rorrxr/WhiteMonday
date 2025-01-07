cda13086981c1899ed50e33132128d735352355e
muchluvu <rorrxr@naver.com>
2025-01-07 20:56:21 +0900

    refactor: wishlist의 feign client를 위한 Product-service 코드 개선 및 추가

--- product-service/src/main/java/com/minju/product/controller/ProductController.java
+++ product-service/src/main/java/com/minju/product/controller/ProductController.java
@@ -1,7 +1,10 @@
 package com.minju.product.controller;
 
+import com.minju.product.dto.ProductDto;
 import com.minju.product.dto.ProductRequestDto;
 import com.minju.product.dto.ProductResponseDto;
+import com.minju.product.entity.Product;
+import com.minju.product.repository.ProductRepository;
 import com.minju.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -15,6 +18,7 @@ import java.util.List;
 @RequestMapping("/api/products")
 public class ProductController {
     private final ProductService productService;
+    private final ProductRepository productRepository;
 
     // 상품 전체 조회하기
     @GetMapping
@@ -24,16 +28,32 @@ public class ProductController {
     }
 
     // 상품 상세 조회하기
-    @GetMapping("/{id}")
-    public ResponseEntity<ProductResponseDto> getProductById(@PathVariable Long id) {
-        ProductResponseDto product = productService.getProductById(id);
-        return ResponseEntity.ok(product);
-    }
+//    @GetMapping("/{id}")
+//    public ResponseEntity<ProductResponseDto> getProductById(@PathVariable Long id) {
+//        ProductResponseDto product = productService.getProductById(id);
+//        return ResponseEntity.ok(product);
+//    }
 
     // 상품 등록
     @PostMapping
     public ResponseEntity<ProductResponseDto> addProduct(@RequestBody ProductRequestDto requestDto) {
         ProductResponseDto savedProduct = productService.addProduct(requestDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedProduct);
+    }
+
+    @GetMapping("/api/products/{productId}")
+    public ResponseEntity<ProductDto> getProductById(@PathVariable Long productId) {
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new IllegalArgumentException("상품을 찾을 수 없습니다."));
+
+        ProductDto productDto = new ProductDto();
+        productDto.setProductId(product.getId());
+        productDto.setTitle(product.getTitle());
+        productDto.setDescription(product.getDescription());
+        productDto.setPrice(product.getPrice());
+        productDto.setStock(product.getStock());
+        productDto.setFlashSale(productDto.isFlashSale());
+
+        return ResponseEntity.ok(productDto);
     }
 }
