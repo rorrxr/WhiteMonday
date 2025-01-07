95f924557cb974f05f34bfee2c4f1289132191ab
muchluvu <rorrxr@naver.com>
2025-01-08 00:57:57 +0900

    chore: 클라우드 버전 프로젝트 설정 수정

--- wishlist-service/src/main/java/com/minju/wishlist/WishlistApplication.java
+++ wishlist-service/src/main/java/com/minju/wishlist/WishlistApplication.java
@@ -1,18 +1,32 @@
 package com.minju.wishlist;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.ApplicationContext;
 
+@EnableFeignClients(basePackages = "com.minju.wishlist.client")
 @SpringBootApplication
-@EnableDiscoveryClient
-@EnableFeignClients
-public class WishlistApplication {
+public class WishlistApplication implements CommandLineRunner {
 
+	private final ApplicationContext context;
+
+	public WishlistApplication(ApplicationContext context) {
+		this.context = context;
+	}
+	@Override
+	public void run(String... args) {
+		// ProductServiceClient Bean 확인
+		if (context.containsBean("productServiceClient")) {
+			System.out.println("잘됨");
+		} else {
+			System.out.println("ㅅㅂ");
+		}
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(WishlistApplication.class, args);
 	}
+
 
 }
