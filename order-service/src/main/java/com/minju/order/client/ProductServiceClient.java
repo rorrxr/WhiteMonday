858e4a2cf857b0facea59c3deea4a74206e69fc9
muchluvu <rorrxr@naver.com>
2025-01-24 01:24:04 +0900

    refactor: feign client config 추가

--- order-service/src/main/java/com/minju/order/client/ProductServiceClient.java
+++ order-service/src/main/java/com/minju/order/client/ProductServiceClient.java
@@ -1,11 +1,12 @@
 package com.minju.order.client;
 
+import com.minju.common.config.FeignConfig;
 import com.minju.common.dto.ProductDto;
 import com.minju.order.dto.StockRequestDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
-@FeignClient(name = "product-service", url = "http://localhost:8082")
+@FeignClient(name = "product-service", configuration = FeignConfig.class)
 public interface ProductServiceClient {
 
     @GetMapping("/api/products/{productId}")
--- wishlist-service/src/main/java/com/minju/wishlist/client/ProductServiceClient.java
+++ wishlist-service/src/main/java/com/minju/wishlist/client/ProductServiceClient.java
@@ -1,12 +1,12 @@
 package com.minju.wishlist.client;
 
+import com.minju.common.config.FeignConfig;
 import com.minju.common.dto.ProductDto;
-import com.minju.wishlist.config.FeignConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "product-service", url = "http://localhost:8082")
+@FeignClient(name = "product-service", configuration = FeignConfig.class)
 public interface ProductServiceClient {
 
     // 상품 상세 조회
