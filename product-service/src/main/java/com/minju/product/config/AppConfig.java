416f71226d7aa2a20effce579da639da3b1d65e2
rorrxr <rorrxr@naver.com>
2025-06-27 18:48:50 +0900

    feat: common 모듈 인식 AppConfig 추가

---
+++ product-service/src/main/java/com/minju/product/config/AppConfig.java
@@ -0,0 +1,9 @@
+package com.minju.product.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.minju.common")
+public class AppConfig {
+}
