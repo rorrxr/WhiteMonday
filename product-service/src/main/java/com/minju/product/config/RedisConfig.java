e20fae9b93ad57385528682e5fa089937058fe4e
rorrxr <rorrxr@naver.com>
2025-07-03 21:13:06 +0900

    refactor: Redis config 리팩토링

--- product-service/src/main/java/com/minju/product/config/RedisConfig.java
+++ product-service/src/main/java/com/minju/product/config/RedisConfig.java
@@ -14,14 +14,19 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisConfig {
 
     @Bean
-    public RedisTemplate<String, Integer> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+    public RedisTemplate<String, Integer> integerRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, Integer> redisTemplate = new RedisTemplate<>();
         redisTemplate.setKeySerializer(new StringRedisSerializer()); // 키는 String으로 직렬화
         redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer()); // 값은 JSON으로 직렬화
         redisTemplate.setConnectionFactory(redisConnectionFactory);
         return redisTemplate;
     }
-
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        return template;
+    }
     @Bean
     public RedissonClient redissonClient() {
         Config config = new Config();
